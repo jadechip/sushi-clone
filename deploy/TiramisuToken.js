841cb9ca4a94dd6f9886ba05a01b14a87c95c501
Jim Eric Skogman <jim.eric.skogman@gmail.com>
2021-06-17 22:07:48 +0700

    Add LavaCake contracts

---
+++ deploy/TiramisuToken.js
@@ -0,0 +1,14 @@
+ module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy } = deployments
+
+  const { deployer } = await getNamedAccounts()
+
+  await deploy("TiramisuToken", {
+    from: deployer,
+    log: true,
+    deterministicDeployment: false
+  })
+}
+
+module.exports.tags = ["TiramisuToken"]
+module.exports.dependencies = ["UniswapV2Factory", "UniswapV2Router02"]
